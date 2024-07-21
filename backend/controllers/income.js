012e41a441b123e58b03b389113bb9d3b5377f8c
alinnecardoso <dev14cardoso@gmail.com>
2024-07-21 18:48:37 -0300

    "Added addIncome function in income.js controller - registrar receita"

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -1,7 +1,36 @@
 const incomeSchema = require("../models/IncomeModel");
 
+
+//Registrar Receita
 exports.addIncome = async (req, res) =>{
+  // Desestrutura o objeto "req.body" para extrair os valores
   const {title, amount, category, description, date} = req.body;
 
-  const income = incomeSchema
+  // Cria um novo objeto com os valores extraídos acima, utilizando o modelo de dados "incomeSchema"
+  const income = incomeSchema({
+    title,
+    amount,
+    category,
+    description,
+    date
+  })
+
+  try {
+    //avalidations
+    if(!title || !category || !description || !date){
+      return res.status(400).json({message: 'All fields are required!'})
+    }
+    if(amount <= 0 || !amount === 'number'){
+      return res.status(400).json({message: 'Amount must be a positive number'})
+    }
+    // Salva o objeto "income" no banco de dados
+    await income.save();
+
+    // Retorna uma resposta 200 com uma mensagem de sucesso
+    res.status(200).json({message: 'Income Added'})
+  } catch (error) {
+    res.status(500).json({message: 'Server Error'})
+  }
+  console.log(income)
 }
+
