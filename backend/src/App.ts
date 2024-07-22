2a8b3367dc18a6d2507c6329433d68df1754874b
alinnecardoso <dev14cardoso@gmail.com>
2024-07-21 21:15:40 -0300

    Transicionando para TS

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
-import { db } from './db/db';
+import db  from './db/db';
 import { readdirSync } from 'fs';
 import dotenv from 'dotenv';
 
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-readdirSync('./routes').map((route: string) => app.use('/api/v1', require(`./routes/${route}`)));
+readdirSync('./routes').map((route: string) => app.use('/api/v1', require(`./routes${route}`)));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
