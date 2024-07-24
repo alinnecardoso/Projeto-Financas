e4e9a004acd4a46a3266063a08a9b543218c1589
alinnecardoso <dev14cardoso@gmail.com>
2024-07-24 10:44:22 -0300

    "Updated Form component with input fields, date picker, and submit functionality; added Form component to Incomes page"

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,9 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { useGlobalContext } from '../../context/GlobalContext';
+
 
 function Form() {
+  const {addIncome} = useGlobalContext()
+
+  const [inputState, setInputState] = useState({
+    title: '',
+    amount: '',
+    date: '',
+    category: '',
+    description: '',
+  })
+
+  const { title, amount, date, category, description } = inputState;
+
+  const handleInput = name => e => {
+    setInputState({...inputState, [name]: e.target.value})
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    addIncome(inputState)
+  }
   return (
-    <div>Form</div>
+    <FormStyled onSubmit={handleSubmit}>
+      <div className="input-control">
+        <input 
+          type="text" 
+          value={title}
+          name={'title'}
+          placeholder='Titulo Salário'
+          onChange={handleInput('title')}
+        />
+      </div>
+
+      <div className="input-control">
+        <input 
+          type="text" 
+          value={amount}
+          name={'amount'}
+          placeholder='R$ 0,00'
+          onChange={handleInput('amount')}
+        />
+      </div>
+
+      <div className="input-control">
+        <DatePicker 
+          id='date'
+          placeholderText='Insira a Data'
+          selected={date} // Mostra a data selecionada
+          dateFormat='dd/MM/yyyy'
+          onChange={(date)=>{// Função executada quando a data muda
+            setInputState({...inputState, date: date}) // Atualiza o estado com a nova data
+          }}
+          />
+      </div>
+
+      <div className="selects input-control">
+          <select required value={category} name="category" id="category" onChange={handleInput('category')}>
+            <option value="" disabled>Selecione uma opção</option>
+            <option value="salary">Salário</option>
+            <option value="freelancing">Freelancing</option>
+            <option value="investments">Investimentos</option>
+            <option value="stocks">Estoques</option>
+            <option value="bitcoin">Bitcoin</option>
+            <option value="bank">Banco</option>
+            <option value="youtube">Youtube</option>
+            <option value="other">Outro</option>
+          </select>
+      </div>
+
+      <div className="input-control">
+        <textarea name="description" value={description} placeholder='Insira a Descrição' cols='30' rows='4' id="description" onChange={handleInput('description')}></textarea>
+      </div>
+
+      <div className="submit-btn">
+        <button>Adicionar Receita</button>
+      </div>
+    </FormStyled>
   )
 }
+
+const FormStyled = styled.form`
+
+`
 
 export default Form
--- frontend/src/components/Pages/Incomes/Incomes.js
+++ frontend/src/components/Pages/Incomes/Incomes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { InnerLayout } from '../../../styles/Layout';
 import { useGlobalContext } from '../../../context/GlobalContext';
+import Form from '../../Form/Form';
 
 function Incomes() {
   const {addIncome} = useGlobalContext()
@@ -11,7 +12,7 @@ function Incomes() {
         <h1>Receita</h1>
         <div className="income-content">
           <div className="form-container">
-
+              <Form/>
           </div>
           <div className="incomes">
 
